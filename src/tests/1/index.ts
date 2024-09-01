3b63dfc81b6064fe7ee8d40fa51fe65e416233a3
Bruno Dias <brunodias40@gmail.com>
2024-09-01 14:47:25 -0300

    feat: add test 1

---
+++ src/tests/1/index.ts
@@ -0,0 +1,14 @@
+console.log(`----------------- Teste (1) ------------------`);
+
+let indice: number = 13;
+let soma: number = 0;
+let k: number = 0;
+
+while (k < indice) {
+  k = k + 1;
+  soma = soma + k;
+}
+
+console.log(`Valor de soma: ${soma}`); // 91
+
+console.log(`----------------------------------------------`);
